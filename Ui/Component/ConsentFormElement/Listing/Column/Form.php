b041742d1cde56308f3281f755f551f60d4619c9
bfluks <bartosz.fluks@endora.pl>
2024-10-08 22:01:34 +0200

    Fix consent form element column form constructor

--- Ui/Component/ConsentFormElement/Listing/Column/Form.php
+++ Ui/Component/ConsentFormElement/Listing/Column/Form.php
@@ -8,6 +8,8 @@
 namespace ExpertSender\Ecdp\Ui\Component\ConsentFormElement\Listing\Column;
 
 use ExpertSender\Ecdp\Api\Data\ConsentFormElementInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
 
 class Form extends Column
@@ -19,9 +21,24 @@ class Form extends Column
 
     /**
      * @param \ExpertSender\Ecdp\Model\Consent\FormElement\Form $form
+     * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
+     * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
+     * @param array $components
+     * @param array $data
      */
-    public function __construct(\ExpertSender\Ecdp\Model\Consent\FormElement\Form $form)
-    {
+    public function __construct(
+        \ExpertSender\Ecdp\Model\Consent\FormElement\Form $form,
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components,
+        array $data
+    ) {
+        parent::__construct(
+            $context,
+            $uiComponentFactory,
+            $components,
+            $data
+        );
         $this->form = $form;
     }
 
